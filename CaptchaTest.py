5459a3a0209003768fa50158f65f6c9ad722232c
JasperH <jasperlhill@gmail.com>
2020-01-18 20:05:24 -0500

    Added preprocessing blocks

--- CaptchaTest.py
+++ CaptchaTest.py
@@ -1,45 +1,73 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import pathlib
 import string
 import re
 import os
+import string
 import numpy             as np
 import tensorflow        as tf
+import tensorflow.keras
 import matplotlib.pyplot as plt
 
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-cwd = os.getcwd()
-captcha_dir = os.path.join(cwd,'captcha_samples')
+URL = 'https://www.researchgate.net/profile/Rodrigo_Wilhelmy/publication/248380891_captcha_dataset/data/00b4951ddc422dddad000000/captcha-dataset.zip'
+data_dir = tf.keras.utils.get_file(fname='captcha_images.zip',origin=URL)
+data_dir = pathlib.Path(data_dir)
+data_dir = data_dir.parents[0]
+#data_dir = os.path.join(data_dir,'samples')
+data_dir = pathlib.Path(data_dir)
 
-train_dir   = os.path.join(captcha_dir,'training_samples')
-test_dir    = os.path.join(captcha_dir,'testing_samples')
+jpg_count = len(list(data_dir.glob('samples/*.jpg')))
+png_count = len(list(data_dir.glob('samples/*.png')))
 
-train_size  = len(os.listdir(train_dir))
-test_size   = len(os.listdir(test_dir))
+print('jpg_count: {}, png_count: {}'.format(jpg_count, png_count))
+
+
 
 #############################################################
-## create labels from file names
+## create label, image pairs
 #############################################################
-train_files = os.listdir(train_dir)
-test_files = os.listdir(test_dir)
-train_labels = []
-test_labels = []
+
+IMG_WIDTH = 200
+IMG_HEIGHT = 50
 
 
-for file in train_files:
-    if re.search('png',file) is not None:
-        train_labels.append(file.replace('.png',''))
+def generate_label(path):
+    parts = tf.strings.split(path,os.path.sep)
+    raw_label = parts[-1]
+    parts = tf.strings.split(raw_label,'.')
+    type = parts[-1]
+    label = parts[0]
+    print('label: {}, type: {}'.format(label,type))
+    return label, type
 
-    elif re.search('jpg',file) is not None:
-        train_labels.append(file.replace('.jpg',''))
+def decode_img(img,type):
+    img = tf.cond(tf.equal(tf.strings.regex_full_match(type,'png'),tf.constant(True)),
+                  tf.image.decode_png(img,channels=3),
+                  tf.image.decode_jpeg(img,channels=3)
+                  )
 
-for file in test_files:
-    if re.search('png',file) is not None:
-        test_labels.append(file.replace('.png',''))
+    img = tf.image.convert_image_dtype(img,tf.float32)
+    return tf.image.resize(img, [IMG_WIDTH,IMG_HEIGHT])
 
-    elif re.search('jpg',file) is not None:
-        test_labels.append(file.replace('.jpg',''))
+def process_path(path):
+    label, type = generate_label(path)
+    img = tf.io.read_file(path)
+    img = decode_img(img,type)
+
+    return label,img
+
+##
+
+list_ds = tf.data.Dataset.list_files([str(data_dir/'*/*.jpg'),str(data_dir/'*/*.png')])
+labeled_ds = list_ds.map(process_path)
+
+for label, raw_img in labeled_ds.take(1):
+    print(repr(raw_img.numpy()[:100]))
+    print('')
+    print(label.numpy())
 
 #############################################################
 ## map the captcha strings to vectors
@@ -57,15 +85,10 @@ def vectorize(string):
     for char in string:
         n = map_char(char)
         v[d] = n
-        d++
+        d += 1
 
-return v/D
+    return v/D
 
-for label in train_labels:
-    vectorize(label)
-
-for label in test_labels:
-    vectorize(label)
 
 #############################################################
 ## preprocess captcha images
@@ -81,13 +104,15 @@ train_image_generator = ImageDataGenerator(rescale=1/255)
 test_image_generator = ImageDataGenerator(rescale=1/255)
 
 train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
-                                                           director=train_dir,
+                                                           directory=data_dir,
                                                            shuffle=False,
-                                                           target_size=(IMG_HEIGHT,IMG_WIDTH)
+                                                           target_size=(IMG_HEIGHT,IMG_WIDTH),
                                                            )
 
 test_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE,
-                                                           director=test_dir,
-                                                           shuffle=False,
-                                                           target_size=(IMG_HEIGHT,IMG_WIDTH)
-                                                           )
+                                                          directory=data_dir,
+                                                          shuffle=False,
+                                                          target_size=(IMG_HEIGHT,IMG_WIDTH),
+                                                          )
+
+
