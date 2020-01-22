51167b952a38beaa246e6030ea6170f145096d6f
JasperH <jasperlhill@gmail.com>
2020-01-21 20:34:34 -0500

    Completed preprocessing block; added tentative network assembly and training blocks

--- CaptchaTest.py
+++ CaptchaTest.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models              import Sequential
-from tensorflow.keras.layers              import Conv2D,MaxPooling2D,Dense,Flatten
+from tensorflow.keras.layers              import Conv2D,MaxPooling2D,Dense,Flatten,Reshape
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -48,18 +48,18 @@ IMG_HEIGHT  =  50
 IMG_WIDTH   = 200
 
 
-## label generation
+## label generation and vectorization ##
 
 alphanumeric = string.digits + string.ascii_lowercase
 alphanum_vec = tf.io.decode_raw(alphanumeric,tf.uint8).numpy()
 D = len(alphanumeric)
-N = 5
+N = 5 ## number of characters in each captcha title
 
 def char_to_vec(char):
     vec = np.zeros(D)
     match = re.search(char,alphanumeric)
     vec[match.span()[0]] = 1
-    return match.span()[0]
+    return vec
 
 def string_to_mat(string):
     N = len(string)
@@ -73,11 +73,11 @@ def string_to_mat(string):
     return mat
 
 def uint8_to_mat(vec): ##transforms vectors from tf.io.decode_raw to sparse matrices    
-    mat = np.zeros([N,D])
+    mat = tf.zeros([D,N])
 
     for i in vec:
-        for j in range(D):
-            if (i == alphanum_vec[j]):
+        for j in D:
+            if (i == alpha_num_vec[j]):
                 mat[i][j] = 1
                 break
     return mat
@@ -85,27 +85,25 @@ def uint8_to_mat(vec): ##transforms vectors from tf.io.decode_raw to sparse matr
 def mat_to_string(mat): ##transforms vector outputs from the network back to strings for visualization
      string = ''
      npmat = mat.numpy()
-     print('npmat is ',npmat)
+
      for i in range(N):
          for j in range(D):
-             if (npmat[i] == alphanum_vec[j]):
+             if (npmat[i][j] == 1):
                  string += alphanumeric[j]
                  break
 
      return string
 
-@tf.function
-def generate_labels(path):    
-    parts = tf.strings.split(path,os.path.sep)
-    raw_label = parts[-1]
-    parts = tf.strings.split(raw_label,'.')
-    string_label = parts[0] ## string_label is a tensor of the form b'true_label'
+def generate_labels(filenames):
+    mat_labels = []
+    for file in filenames:
+        parts = re.split('\.',file)
+        string_label = parts[0] ## string_label is a tensor of the form b'true_label'
+        mat_label = string_to_mat(string_label)        
+        mat_labels.append(mat_label)
+    return mat_labels
 
-    vectorized_label = tf.io.decode_raw(string_label,tf.uint8)
-    #mat_label = uint8_to_mat(vectorized_label)
-    return vectorized_label
-
-## image generation
+## image generation ##
 
 def decode_png(png):
     png = tf.image.decode_png(png,channels=3)
@@ -118,33 +116,43 @@ def decode_jpg(jpg):
     return tf.image.resize(jpg, [IMG_HEIGHT,IMG_WIDTH])
 
 def process_path_png(path):
-    mat_label = generate_labels(path)
+    #mat_label = generate_labels(path)
     img = tf.io.read_file(path)
     img = decode_png(img)
 
-    return img,mat_label
+    return img
 
 def process_path_jpg(path):
-    mat_label = generate_labels(path)
+    #mat_label = generate_labels(path)
     img = tf.io.read_file(path)
     img = decode_jpg(img)
     
-    return img,mat_label
+    return img
 
 
 #############################################################
 ## compile dataset
 #############################################################
 
-list_png_ds = tf.data.Dataset.list_files(str(data_dir/'*/*.png'))
-list_jpg_ds = tf.data.Dataset.list_files(str(data_dir/'*/*.jpg'))
-labeled_png_ds = list_png_ds.map(process_path_png,num_parallel_calls=AUTOTUNE)
-labeled_jpg_ds = list_jpg_ds.map(process_path_jpg,num_parallel_calls=AUTOTUNE)
-full_ds = tf.data.Dataset.concatenate(labeled_png_ds,labeled_jpg_ds)
-train_ds = full_ds.shard(num_shards=2, index=0)
-test_ds = full_ds.shard(num_shards=2, index=1)
+list_png_ds = tf.data.Dataset.list_files(str(data_dir/'*/*.png'),shuffle=False)
+list_jpg_ds = tf.data.Dataset.list_files(str(data_dir/'*/*.jpg'),shuffle=False)
+png_ds = list_png_ds.map(process_path_png,num_parallel_calls=AUTOTUNE)
+jpg_ds = list_jpg_ds.map(process_path_jpg,num_parallel_calls=AUTOTUNE)
+img_ds = tf.data.Dataset.concatenate(png_ds,jpg_ds)
+#train_ds = full_ds.shard(num_shards=2, index=0)
+#test_ds = full_ds.shard(num_shards=2, index=1)
 
-EPOCHS      = 150
+filenames = os.listdir(data_dir/'samples')
+filenames.sort()
+mat_labels = generate_labels(filenames)
+mat_ds = tf.data.Dataset.from_tensor_slices(mat_labels)
+
+full_ds = tf.data.Dataset.zip((img_ds,mat_ds))
+ds = full_ds.enumerate(start=0)
+train_ds = full_ds.shard(2,0)
+test_ds = full_ds.shard(2,1)
+
+EPOCHS      = 15
 BATCH_SIZE  = 128
 STEPS_PER_EPOCH = np.ceil(0.5*NUM_OF_IMAGES/BATCH_SIZE) ## factor of 0.5 is for the validation split
 
@@ -183,7 +191,7 @@ show_batch(img_batch.numpy(),label_batch)
 test_ds = prep_for_training(test_ds)
 ## auxiliary function to quantify dataset performance
 default_timeit_steps = 1000
-a = tf.io.decode_raw()
+
 def timeit(ds, steps=default_timeit_steps):
     start = time.time()
     it = iter(ds)
@@ -204,17 +212,18 @@ timeit(train_ds)
 ## build and train the model
 #############################################################
 
-model = Sequential([Conv2D(10, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH,3)),
+model = Sequential([Conv2D(20, kernel_size=(20,80), padding='same', activation='relu', input_shape=(IMG_HEIGHT,IMG_WIDTH,3)),
                     MaxPooling2D(2,2),
-                    Conv2D(20, 3, padding='same', activation='relu'),
+                    Conv2D(40, kernel_size=(10,40), padding='same', activation='relu'),
                     MaxPooling2D(2,2),
-                    Conv2D(40, 3, padding='same', activation='relu'),
+                    Conv2D(80, 3, padding='same', activation='relu'),
                     MaxPooling2D(2,2),
                     Flatten(),
-                    Dense(1000, activation='sigmoid'),
-                    Dense(5, activation=None)])
+                    Dense(180, activation='relu'),
+                    Reshape((5,36),input_shape=(180,)),
+                    ])
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 history = model.fit(train_ds,
                     validation_data=test_ds,
@@ -227,4 +236,4 @@ history = model.fit(train_ds,
 
 labels = model.predict(test_ds,steps=1)
 
-print(labels)
+
